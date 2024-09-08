f9e25115d80342e40365597a23f533fc2c29d61d
I am Jack <74429355+Jack-chau@users.noreply.github.com>
2024-09-08 23:57:50 +0800

    adding text

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,40 @@ class JackCard extends StatelessWidget{
 
   Widget build( BuildContext context ){
     return Scaffold(
+      backgroundColor: Colors.grey[ 900 ],
       appBar: AppBar(
-        title: const Text( 'Jack Id Card' ),
+        title: const Text( 'Jack ID Card', style: TextStyle( color: Colors.white ) ),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 229, 138, 175),
+        backgroundColor: Colors.grey[ 850 ],
+        elevation: 0,
       ),
+
+      body: const Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0 ),
+        child : Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text( 'NAME', style: TextStyle( color: Colors.grey, letterSpacing: 2.0 ) ),
+            SizedBox( height: 5.0 ),
+            Text( 'Jack-Chau', style: TextStyle( color: Color.fromARGB(255, 255, 215, 64), letterSpacing: 2.0, fontSize: 20.0, fontWeight: FontWeight.bold ) ),
+            SizedBox( height: 20.0 ),
+
+            Text( 'Current Status', style: TextStyle( color: Colors.grey, letterSpacing: 2.0 ) ),
+            SizedBox( height: 5.0 ),
+            Text( 'Single and Rish', style: TextStyle( color: Color.fromARGB(255, 255, 215, 64), letterSpacing: 2.0, fontSize: 20.0, fontWeight: FontWeight.bold ) ),
+            SizedBox( height: 30.0 ),
+
+            Row(
+              children: [
+                Icon( Icons.email, color: Colors.blue ),
+                SizedBox( width: 10 ),
+                Text( 'chaukaho149@gmail.com', style: TextStyle( color: Color.fromARGB(255, 148, 182, 210), letterSpacing: 1, ), ),
+              ],
+            )
+          ]
+        )
+      ),
+
     );
   }
 }
