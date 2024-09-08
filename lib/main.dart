47cd3f14115f9648f1040d45b2580e6bbf17f3fc
I am Jack <74429355+Jack-chau@users.noreply.github.com>
2024-09-09 00:31:33 +0800

    'add_icon'

--- lib/main.dart
+++ lib/main.dart
@@ -3,50 +3,105 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: JackCard(),
+      home: AboutJack(),
     ),
   );
 }
 
-class JackCard extends StatelessWidget{
+class AboutJack extends StatelessWidget{
   @override
 
   Widget build( BuildContext context ){
     return Scaffold(
       backgroundColor: Colors.grey[ 900 ],
       appBar: AppBar(
-        title: const Text( 'Jack ID Card', style: TextStyle( color: Colors.white ) ),
+        title: const Text( 'About Jack', style: TextStyle( color: Colors.white ) ),
         centerTitle: true,
         backgroundColor: Colors.grey[ 850 ],
         elevation: 0,
       ),
 
       body: const Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0 ),
+        padding: EdgeInsets.fromLTRB(30, 20, 30, 20 ),
         child : Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text( 'NAME', style: TextStyle( color: Colors.grey, letterSpacing: 2.0 ) ),
+            Center(
+              child: CircleAvatar( 
+                backgroundImage: AssetImage('images/MeMe.jpg'), 
+                radius: 40 
+              )
+            ),
+
+            Divider( 
+              height: 50, 
+              color: Color.fromARGB(42, 211, 208, 208) 
+            ),
+            
+            Text( 
+              'NAME', 
+              style: TextStyle( 
+                color: Colors.grey, 
+                letterSpacing: 2.0 
+              ) 
+            ),
+
             SizedBox( height: 5.0 ),
-            Text( 'Jack-Chau', style: TextStyle( color: Color.fromARGB(255, 255, 215, 64), letterSpacing: 2.0, fontSize: 20.0, fontWeight: FontWeight.bold ) ),
+
+            Text( 
+              'Jack-Chau', 
+              style: TextStyle( 
+                color: Color.fromARGB(255, 255, 215, 64), 
+                letterSpacing: 2.0, 
+                fontSize: 20.0, 
+                fontWeight: FontWeight.bold 
+              ) 
+            ),
+
             SizedBox( height: 20.0 ),
 
-            Text( 'Current Status', style: TextStyle( color: Colors.grey, letterSpacing: 2.0 ) ),
+            Text( 
+              'Current Status', 
+              style: TextStyle( 
+                color: Colors.grey, 
+                letterSpacing: 2.0 
+              ) 
+            ),
+
             SizedBox( height: 5.0 ),
-            Text( 'Single and Rish', style: TextStyle( color: Color.fromARGB(255, 255, 215, 64), letterSpacing: 2.0, fontSize: 20.0, fontWeight: FontWeight.bold ) ),
+            
+            Text( 
+              'Single and Rish', 
+              style: TextStyle( 
+                color: Color.fromARGB(255, 255, 215, 64), 
+                letterSpacing: 2.0, 
+                fontSize: 20.0, 
+                fontWeight: FontWeight.bold 
+              ) 
+            ),
             SizedBox( height: 30.0 ),
 
             Row(
               children: [
-                Icon( Icons.email, color: Colors.blue ),
+                Icon( 
+                  Icons.email, 
+                  color: Colors.blue 
+                ),
+
                 SizedBox( width: 10 ),
-                Text( 'chaukaho149@gmail.com', style: TextStyle( color: Color.fromARGB(255, 148, 182, 210), letterSpacing: 1, ), ),
+
+                Text( 
+                  'chaukaho149@gmail.com', 
+                  style: TextStyle( 
+                    color: Color.fromARGB(255, 148, 182, 210), 
+                    letterSpacing: 1, 
+                  ), 
+                ),
               ],
             )
           ]
         )
       ),
-
     );
   }
 }
